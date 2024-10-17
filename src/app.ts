ac81d87b366f0d33263e8a1c830dbf0f7ddc4ce7
Nikhil Garg <nikhil@gargmail.com>
2024-10-17 16:39:45 -0700

    add authorized users

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,18 @@
 import express, { Request, Response } from 'express';
 import dotenv from 'dotenv';
+import fs from 'fs';
+import path from 'path';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+
+const authorizedUsers: string[] = JSON.parse(
+  fs.readFileSync(path.join(__dirname, '..', 'authorized_users.json'), 'utf-8')
+);
+
 
 app.use(express.json());
 
@@ -12,7 +20,14 @@ app.get('/', async (req: Request, res: Response) => {
     const alphaAdvantageUrl = 'https://www.alphavantage.co/query';
     const apiKey = process.env.ALPHA_ADVANTAGE_API_KEY;
     // get request params
-    const queryParams = {...req.query, apikey: apiKey};
+    const query = req.query;
+    
+    if (!query.username || !authorizedUsers.includes(query.username as string)) {
+        res.status(401).send('Unauthorized');
+        return;
+    }
+
+    const queryParams = {...query, apikey: apiKey};
     // make request to alpha vantage
     const response = await fetch(`${alphaAdvantageUrl}?${new URLSearchParams(queryParams as Record<string, string>)}`);
     const data = await response.json();
