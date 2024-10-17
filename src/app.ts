8c8a5a6f994af4dd91a5e6b989d005150bf0e006
Nikhil Garg <nikhil@gargmail.com>
2024-10-17 15:59:28 -0700

    remove prints

--- src/app.ts
+++ src/app.ts
@@ -11,13 +11,11 @@ app.use(express.json());
 app.get('/', async (req: Request, res: Response) => {
     const alphaAdvantageUrl = 'https://www.alphavantage.co/query';
     const apiKey = process.env.ALPHA_ADVANTAGE_API_KEY;
-    console.log(apiKey);
     // get request params
     const queryParams = {...req.query, apikey: apiKey};
     // make request to alpha vantage
     const response = await fetch(`${alphaAdvantageUrl}?${new URLSearchParams(queryParams as Record<string, string>)}`);
     const data = await response.json();
-    console.log(data);
     res.send(data);
 });
 
